059610d51be5d872b0b68b4e29a0299ec7b7c5f1
Edward Ezekiel <ed.a.ezekiel@gmail.com>
2018-11-26 16:18:48 -0500

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,5 +2,6 @@ class PostsController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   def show
+    @post = Post.find(params[:id])
   end
 end
